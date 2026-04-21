37530c52a92e7c051f790abc81c5f3bd647f8ba8
Spandanbalabantaray <spandanb.bsc23@rvu.edu.in>
2026-04-21 10:58:27 +0530

    UI added

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
+donor_list = ["A+", "B+", "O-"]
+
 @app.route('/')
 def home():
-    return jsonify({"message": "Blood Donation Management System Running"})
+    return render_template("index.html", donors=donor_list)
 
-@app.route('/donors')
-def donors():
-    return jsonify({"donors": ["A+", "B+", "O-"]})
+@app.route('/add', methods=['POST'])
+def add_donor():
+    blood = request.form.get("blood_group")
+    if blood:
+        donor_list.append(blood)
+    return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render dynamic port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
